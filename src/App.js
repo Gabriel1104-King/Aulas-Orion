b839eca067dbbdcb75aaf9b2fa1af1fbe9bcf770
Gabriel Reis <61297917+Gabriel1104-King@users.noreply.github.com>
2022-03-23 19:45:18 -0300

    primeiro up

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/Menu';
+import { Button } from 'bootstrap';
+
+function App() {
+  return (
+    <div>
+      
+      <Menu>
+        
+      </Menu>
+          <h1>Hello World</h1>
+
+          <Button variant='primary'>Primary</Button>
+     </div>  
+  );
+}
+
+export default App;
